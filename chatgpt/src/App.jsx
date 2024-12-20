e8baf770442e813bc17c2529f23a19a48b80e5d5
kirannjr11 <ghalekiran03@gmail.com>
2024-12-20 19:23:59 +0900

    completed initial design

--- chatgpt/src/App.jsx
+++ chatgpt/src/App.jsx
@@ -6,23 +6,30 @@ import message from './assets/message.png'
 
 
 function App() {
-  
+
   return (
-   <div className="App">
+    <div className="App">
       <div className="sideBar">
         <div className="upperside">
           <div className="uppsersideTop">
-            <img src={chatgpt} alt="image" className="logo"/>
+            <img src={chatgpt} alt="image" className="logo" />
             <span className="brand">ChatGPT</span>
-            </div>
-            <button className="midbtn">
-              <img src={plus}alt='' className='addBtn'/>
-              New Chat
-            </button>
-          
+          </div>
+          <button className="midbtn">
+            <img src={plus} alt='' className='addBtn' />
+            New Chat
+          </button>
+
           <div className="upperSideBottom">
-            <button className="query"><img src={message} />What is programing</button>
-            <button className="query"><img src={message} />Start new Search</button>
+            <button className="msgbtn">
+              <img src={message} alt='' className='addBtn' />
+              What is programming?
+            </button>
+            <button className="msgbtn">
+              <img src={message} alt='' className='addBtn' />
+              Learn new skills
+            </button>
+
           </div>
         </div>
 
@@ -32,9 +39,9 @@ function App() {
       </div>
 
       <div className="main">
-        
+
       </div>
-   </div>
+    </div>
   )
 }
 
